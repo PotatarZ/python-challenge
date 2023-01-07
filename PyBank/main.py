48a70e9d030e2332480cf7094ae68e8e28cf7414
PotatarZ <holgersonpeter@gmail.com>
2023-01-06 23:23:39 -0500

    First draft of analysis program

---
+++ PyBank/main.py
@@ -0,0 +1,69 @@
+# Necessary libraries
+import os
+import csv
+import numpy
+
+# Declare vars
+months_list = []
+profits_list = []
+change_list = []
+previous_profit = 0
+months = int(0)
+total = int(0)
+average = float(0)
+
+# Path to the csv file
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Open and begin reading the csv, skiping the header
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    # Creates actionable lists from the data in the csv file
+    for row in csvreader:
+        months_list.append(row[0])
+        profits_list.append(int(row[1]))
+        ## alternative to numpy.diff below
+        # change_list.append(int(row[1]) - previous_profit)
+        # previous_profit = int(row[1])
+csvfile.close()
+
+# The total number of months included in the dataset
+months = len(months_list)
+
+# The net total amount of "Profit/Losses" over the entire period
+total = sum(profits_list)
+
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes (skips the initial change on month 1)
+change_list = numpy.diff(profits_list).tolist()
+average = round(sum(change_list) / (months-1), 2)
+
+# The greatest increase in profits (date and amount) over the entire period
+max_profit = max(change_list)
+max_date = months_list[change_list.index(max_profit)+1]
+
+# The greatest decrease in profits (date and amount) over the entire period
+min_profit = min(change_list)
+min_date = months_list[change_list.index(min_profit)+1]
+
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+analysis = (
+    "\n"
+    "Financial Analysis\n"
+    "----------------------------\n"
+    f"Total Months: {months}\n"
+    f"Total: ${total}\n"
+    f"Average Change: ${average}\n"
+    f"Greatest Increase in Profits: {max_date} (${max_profit})\n"
+    f"Greatest Decrease in Profits: {min_date} (${min_profit})"
+    "\n"
+)
+
+print(analysis)
+
+outputpath = os.path.join("Analysis", "financial_analysis.txt")
+
+with open(outputpath, "w") as textfile:
+    textfile.write(analysis)
